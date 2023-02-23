a33f30f73eae1719fec82d1f48a69384a34f8cd1
debabrata-97 <debabrataszr@gmail.com>
2023-02-23 19:14:56 +0530

    Welcome to Line Comparison Computation Program

---
+++ src/Line_comparison/LineComparison.java
@@ -0,0 +1,8 @@
+package Line_comparison;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+
+    }
+}
